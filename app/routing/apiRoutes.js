35c9f391bde1db9bcaee4a5ff01ee5a66f8d49c1
Gabriel Figarella <GFigarella@Gabriels-MacBook-Pro.local>
2018-01-19 10:07:37 -0500

    added some comments

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,4 +4,12 @@ module.exports = function(app){
     app.get("/api/friends", function (req, res){
         res.json(scoresArray);
     })
+
+
+    app.post("/api/friends", function(req, res){
+        // logic that pushes the score goes here, jQuery and all that stuff
+        // push survey scores to the scoresArray
+        // compare scores
+        // push back the best-match
+    })
 }
